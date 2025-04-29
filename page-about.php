c34ac8819f12ee5723fc57666f9d6ace944213d9
saresc01 <sara.escallon@gmail.com>
2025-04-29 20:57:20 +0200

    page about done the first and third part

--- page-about.php
+++ page-about.php
@@ -1,5 +1,68 @@
 <?php get_header() ?>
-      
+    <section>
+        <div class="container section-first">
+        </div>
+    </section>
+    
+    <?php if(have_posts()): ?>
+        <?php while(have_posts()): the_post(); ?>
+            <?php
+            $presentation_title = get_field("presentation_title");
+            $presentation_text = get_field("presentation_text");
+            $presentation_img = get_field("presentation_img");
+            $approach_title = get_field("approach_title");
+            $approach_text = get_field("approach_text");
+            $approach_img = get_field("approach_img");            
+            ?>
+
+            <!--PRESENTATION-->
+            <section class="section-personal">
+                <div class="container">
+                    <div class="row">
+                        <!-- Img -->
+                        <div class="col-md-6">
+                            <img src="<?php echo $presentation_img["url"]; ?>" alt="Rasa" class="portrait-image img-fluid">
+                        </div>
+
+                        <!-- Text -->
+                        <div class="col-md-6 d-flex align-items-center">
+                            <div class="text-personal">
+                                <h1><?php echo $presentation_title; ?></h1>
+                                <p><?php echo $presentation_text; ?></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!--END PRESENTATION-->
+
+            <!--SECTION CARE FROM TEMPLATE PARTS-->
+            <?php get_template_part("template-parts/care") ?>
+
+            <!--APPROACH-->
+            <section class="section-aproach">
+                <div class="container">
+                    <div class="row">
+                        <!-- Text -->
+                        <div class="col-md-6 d-flex align-items-center">
+                            <div class="px-">
+                                <h2><?php echo $approach_title; ?></h2>
+                                <p><?php echo $approach_text; ?></p>
+                                <a href="#" class="btn-primary btn">Book a Consultation</a>
+                            </div>
+                        </div>
+                        <!-- Img -->
+                        <div class="col-md-6">
+                            <img src="<?php echo $approach_img["url"]; ?>" alt="Rasa" class="portrait-aproach img-fluid">
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!--END APPROACH-->            
+
+
+    <?php the_content(); ?>
+    <?php endwhile; else: endif; ?>
 
 
 <?php get_footer() ?>
---
+++ template-parts/care.php
@@ -0,0 +1,103 @@
+<!-- CARE SECTION -->
+<section class="section-care">
+        <div class="container">
+            <h2 class="text-center mb-4">WHAT I CARE ABOUT</h2>
+
+            <!-- GRID desktop - ITEM 1 -->
+            <div class="row text-center d-none d-md-flex">
+                <div class="col-md-3 mb-4">
+                    <div class="py-4 px-3 h-100">
+                        <div class="icon-wrapper mb-3">
+                            <i class="bi bi-recycle fs-1 icon-colored"></i>
+                        </div>
+                        <h4>Sustainable Materials</h4>
+                        <p>Working with high-quality, natural, and sustainable fabrics.</p>
+                    </div>
+                </div>
+
+                <!-- ITEM 2 -->
+                <div class="col-md-3 mb-4">
+                    <div class="py-4 px-3 h-100">
+                        <div class="icon-wrapper mb-3">
+                            <i class="bi bi-person-check-fill fs-1 icon-colored"></i>
+                        </div>
+                        <h4>HONEST, PERSONAL CONNECTION</h4>
+                        <p>Every piece begins with a real conversation, not a shopping cart.</p>
+                    </div>
+                </div>
+
+                <!-- ITEM 3 -->
+                <div class="col-md-3 mb-4">
+                    <div class="py-4 px-3 h-100">
+                        <div class="icon-wrapper mb-3">
+                            <i class="bi bi-house-heart-fill fs-1 icon-colored"></i>
+                        </div>
+                        <h4>CONSCIOUS MATERIALS</h4>
+                        <p>Carefully chosen fabrics that feel good, last longer, and respect the planet.</p>
+                    </div>
+                </div>
+
+                <!-- ITEM 4 -->
+                <div class="col-md-3 mb-4">
+                    <div class="py-4 px-3 h-100">
+                        <div class="icon-wrapper mb-3">
+                            <i class="bi bi-check2-square fs-1 icon-colored"></i>
+                        </div>
+                        <h4>MADE TO BE WORN</h4>
+                        <p>Tailored clothing should fit into your real life — not just a runway or one-time event.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- CARRUSEL MOBILE VERSION -->
+            <div id="why-carousel" class="carousel slide d-block d-md-none" data-bs-ride="carousel">
+                <div class="carousel-inner text-center">
+
+                    <div class="carousel-item active">
+                        <div class="py-4 px-3">
+                            <div class="icon-wrapper mb-3">
+                                <i class="bi bi-recycle fs-1 icon-colored"></i>
+                            </div>
+                            <h4>CRAFT OVER SPEED</h4>
+                            <p>Taking the time to do things right - with skill, patience, and full attention to detail.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <div class="py-4 px-3">
+                            <div class="icon-wrapper mb-3">
+                                <i class="bi bi-person-check-fill fs-1 icon-colored"></i>
+                            </div>
+                            <h4>HONEST, PERSONAL CONNECTION</h4>
+                            <p>Every piece begins with a real conversation, not a shopping cart.</p>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <div class="py-4 px-3">
+                            <div class="icon-wrapper mb-3">
+                                <i class="bi bi-house-heart-fill fs-1 icon-colored"></i>
+                            </div>
+                            <h4>CONSCIOUS MATERIALS</h4>
+                            <p>Carefully chosen fabrics that feel good, last longer, and respect the planet.</p>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <div class="py-4 px-3">
+                            <div class="icon-wrapper mb-3">
+                                <i class="bi bi-check2-square fs-1 icon-colored"></i>
+                            </div>
+                            <h4>MADE TO BE WORN</h4>
+                            <p>Tailored clothing should fit into your real life — not just a runway or one-time event.
+                            </p>
+                        </div>
+                    </div>
+
+                </div>                
+
+            </div>
+
+        </div>
+    </section>
